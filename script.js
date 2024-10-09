93f68e2ca1b7bd4869292d3f7d61c87dc7da1c84
Abdalrhman-m <sgud776@gmail.com>
2024-10-09 20:15:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const countdown = () => {
+    const firstAnniversary = new Date('October 10, 2024 00:00:00').getTime();
+    const now = new Date().getTime();
+    const timeDifference = firstAnniversary - now;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    if (timeDifference < 0) {
+        document.getElementById('countdown').innerHTML = "Happy Anniversary!";
+    }
+};
+
+setInterval(countdown, 1000);
